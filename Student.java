ac10f16605c9641b0a56786972a212d604df1911
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2023-09-04 00:15:28 -0600

    1

--- Student.java
+++ Student.java
@@ -29,27 +29,53 @@ public class Student {
         this.email = email;
         this.grades = new ArrayList<Grade>();
     }
+    /**getter
+     * @return nombre
+     */
     public String getName() {
         return name;
     }
+    /**getter lastname
+     * @return apellido
+     */
     public String getLastname() {
         return lastname;
     }
+    /**getter codigo
+     * @return id
+     */
     public int getId() {
         return id;
     }
+    /** getter birthdate
+     * @return birthdate
+     */
     public String getBirthdate() {
         return birthdate;
     }
+    /** getter correo
+     * @return correo
+     */
     public String getEmail() {
         return email;
     }
+    /**getter notas
+     * @return notas
+     */
     public ArrayList<Grade> getGrades() {
         return grades;
     }
+    /**añade una nota al listado de notas del estudiante
+     * @param grade la nota a añadir
+     */
     public void addGrade(Grade grade) {
         grades.add(grade);
     }
+    /** método que retorna las notas del estudiante en cierto lugar y curso
+     * @param campus
+     * @param course
+     * @return las notas obtenidas en el campus y curso especificados
+     */
     public ArrayList<Integer> getGradeGroup(String campus, String course) {
         ArrayList<Integer> scores = new ArrayList<Integer>();
         for (Grade grade : grades) {
--- StudentGrading.java
+++ StudentGrading.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
 
 public class StudentGrading {
     public static void main(String[] args) {
-        ArrayList<Student> students = new ArrayList<Student>();
-        ArrayList<String> campuslist = new ArrayList<String>();
-        ArrayList<String> courses = new ArrayList<String>();
-        courses.add("Matemáticas");
+        ArrayList<Student> students = new ArrayList<Student>(); // Listado de estudiante
+        ArrayList<String> campuslist = new ArrayList<String>(); // listado de campus
+        ArrayList<String> courses = new ArrayList<String>(); // listado de cursos
+        courses.add("Matemáticas");// añadir nombres de cursos
         courses.add("Lenguaje");
         courses.add("Química");
         courses.add("Física");
@@ -21,23 +21,24 @@ public class StudentGrading {
         courses.add("Estadística");
         Scanner s = new Scanner(System.in);
         boolean menu = true;
-        while (menu) {
-            System.out.println("Bienvenido, elija una opción:\n1) Ingresar campus\n2) Ingresar datos de estudiante\n3) Ingresar nota de un estudiante\n 4) Datos estadísticos\n 5)Salir");
+        while (menu) {// menú del programa
+            System.out.println("Bienvenido, elija una opción:\n1) Ingresar campus\n2) Ingresar datos de estudiante\n3) Ingresar nota de un estudiante\n4) Datos estadísticos\n5) Salir");
             switch (s.nextInt()) {
                 case 1:
                 System.out.println("Ingrese el nombre del campus: ");
                 s.nextLine();
-                campuslist.add(s.nextLine());
+                campuslist.add(s.nextLine());// añade campus a la lista
                 break;
                 case 2:
-                System.out.println("Ingrese el nombre del estudiante:");
+                System.out.println("Ingrese el nombre del estudiante:");// solicita la información del estudiante
                 String name = s.nextLine();
+                s.nextLine();
                 System.out.println("Ingrese el apellido del estudiante:");
                 String lastname = s.nextLine();
                 System.out.println("Ingrese el codigo de identificación del estudiante:");
                 boolean cc = true;
                 int id = 0;
-                while (cc){
+                while (cc){// verifica que el código sea único
                     cc = false;
                     id = s.nextInt();
                     for (Student st : students) {
@@ -47,7 +48,7 @@ public class StudentGrading {
                     }
                 }
                 s.nextLine();
-                System.out.println("Ingrese la fecha de nacimiento del estudiante:");
+                System.out.println("Ingrese la fecha de nacimiento del estudiante:");// Añade la fecha preguntando paso a paso
                 System.out.println("Ingrese el día:");
                 int dd = s.nextInt();
                 System.out.println("Ingrese el mes:");
@@ -62,18 +63,18 @@ public class StudentGrading {
                 break;
                 case 3:
                 if (campuslist.size() == 0) {
-                    System.out.println("Ingrese primero un campus");
+                    System.out.println("Ingrese primero un campus");// Verifica que haya campus dentro del programa
                     break;
                 }
                 if (students.size() == 0) {
-                    System.out.println("Ingrese primero un estudiante:");
+                    System.out.println("Ingrese primero un estudiante:");// verifica que haya estudiantes
                     break;
                 }
                 System.out.println("Ingrese el codigo del estudiante:");
                 int gid = s.nextInt();
                 Student gst = new Student("", "", 0, "", "");
                 boolean exist = false;
-                for (Student st : students) {
+                for (Student st : students) {// Verificar estudiante ingresado
                     if (st.getId() == gid) {
                         gst = st;
                         exist = true;
@@ -94,26 +95,19 @@ public class StudentGrading {
                 } while (c < 1 || c > campuslist.size());
                 String campus = campuslist.get(c-1);
                 s.nextLine();
-                System.out.println("Ingrese el nombre del curso del exámen:");
-                String course;
+                System.out.println("Ingrese el número del curso del exámen:");
+                String course; c = 0;
                 for (String x : courses){
-                    System.out.println(x);
+                    System.out.println((c+1) + ". " + x);
+                    c++;
                 }
-                while (true) {
-                    course = s.nextLine();
-                    boolean cond = false;
-                    for (String x: courses){
-                        if (x == course){
-                            cond =true;
-                        }
-                    }
-                    if (cond){
-                        break;
-                    }
-                }
+                do {
+                    c = s.nextInt();
+                } while (c < 1 || c > courses.size());
+                course = courses.get(c - 1);
                 System.out.println("Ingrese la puntuación obtenida:");
                 int score;
-                do {
+                do {// Verificar puntuación entre 0 y 100
                     score = s.nextInt();
                 } while (score < 0 || score > 100);
                 Grade grade = new Grade(campus, course, score);
@@ -125,8 +119,8 @@ public class StudentGrading {
                     System.out.println("Datos estadísticos para " + cmps + ":");
                     for (String crs : courses) {
                         System.out.println(crs + ":");
-                        ArrayList<Integer> lst = new ArrayList<Integer>();
-                        int avg = 0;
+                        ArrayList<Integer> lst = new ArrayList<Integer>();// Lista de notas de todos los estudiantes para el curso
+                        float avg = 0;
                         int sz = 0;
                         float stddev = 0;
                         for (Student st : students){
@@ -137,29 +131,34 @@ public class StudentGrading {
                                 lst.add(gr);
                             }
                         }
-                        Collections.sort(lst);
-                        System.out.println("Promedio: " + (avg/sz));
-                        int max = 0; int tmax = 0; int mode = 0; boolean mdn = true;
-                        for (int i = 1; i < lst.size(); i++) {
-                            if (lst.get(i) == lst.get(i-1)) {
+                        if (lst.size() == 0){
+                            System.out.println("Sin notas");// salir si no hay notas en este curso
+                            continue;
+                        }
+                        Collections.sort(lst);// Ordenar lista de mayor a menor
+                        System.out.println("Promedio: " + (avg/sz));// promedio calculando suma de todo dividido numero total
+                        int max = 0; int tmax = 0; float mode = lst.get(0); boolean mdn = true;
+                        for (int i = 0; i < lst.size(); i++) {// Calcular moda
+                            if (i < lst.size() - 1){
+                            if (lst.get(i) == lst.get(i+1)) {
                                 tmax ++;
-                                if (i == lst.size() - 1) {
+                                if (i == lst.size() - 2) {
                                     if (tmax +1 > max){
-                                    mode = lst.get(i-1);
+                                    mode = lst.get(i);
                                     max = tmax + 1;
                                 }
                                 }
                             }
                             else {
                                 if (tmax +1 > max){
-                                    mode = lst.get(i-1);
+                                    mode = lst.get(i);
                                     max = tmax + 1;
                                 }
                                 tmax = 0;
-                            }
-                            if ((i+1)*2 >= lst.size() && mdn) {
+                            }}
+                            if ((i+1)*2 >= lst.size() && mdn) {// Imprimir mediana verficando que es la de en medio
                                 if ((i+1)*2 == lst.size()) {
-                                    System.out.println("Mediana: " + ((lst.get(i) + lst.get(i+1)) / 2));
+                                    System.out.println("Mediana: " + (float) ((lst.get(i) + lst.get(i + 1)) / 2));
                                 }
                                 else {
                                     System.out.println("Mediana: " + lst.get(i));
@@ -167,17 +166,22 @@ public class StudentGrading {
                                 mdn = false;
                             }
                         }
-                        System.out.println("Moda: " + (mode));
-                        for (int gr : lst){
+                        System.out.println("Moda (excluyente): " + (mode));
+                        for (float gr : lst){// Calcular varianza
                             stddev += (gr - (avg/sz)) * (gr - (avg/sz));
                         }
-                        System.out.println("Desviación estándar: " + (stddev / (sz - 1)));
+                        if (sz > 1) {
+                            System.out.println("Desviación estándar: √" + (stddev / (sz - 1)));// Mostrar desviación estándar
+                        }
+                        else {
+                            System.out.println("Desviación estándar: 0");
+                        }
 
                     }
                 }
                 break;
                 case 5:
-                menu = false;
+                menu = false;// salir
                 break;
                 default:
                 System.out.println("Ingrese una opción correcta");
